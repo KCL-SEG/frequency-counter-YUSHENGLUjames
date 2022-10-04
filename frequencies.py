382b0f80e6373a930e9a39122ab0c66023d14970
YUSHENG LU <yusheng.lu@kcl.ac.uk>
2022-10-04 01:12:53 +0100

    Done

--- frequencies.py
+++ frequencies.py
@@ -1,7 +1,12 @@
 """Frequencies function."""
 """ENTER YOUR SOLUTION HERE!"""
 
-def frequencies(items):
+def frequencies(*items):
     frequencies = {}
     # Your code goes here
+    for object in items:
+        name = str(object)
+        number = items.count(object)
+        frequencies.update({name:number})
+
     return frequencies
